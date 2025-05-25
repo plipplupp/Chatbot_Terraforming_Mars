2c2de5d381af3d9f7fe9074efbfbb56cb721ebff
plipplupp <jeansson@gmail.com>
2025-05-25 11:35:23 +0200

    Fix: Added pysqlite3-binary and updated app.py for sqlite3 compatibility

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 # 1. IMPORTER HÄR. Dessa anropar INTE Streamlit-funktioner direkt.
 import streamlit as st # Denna måste vara här!
 import os
